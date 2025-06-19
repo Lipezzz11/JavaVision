526ee45b13c1c652812b5d6e9c75c94fdcbc66f2
Felipe Soares <felipeirasoares@gmail.com>
2025-06-19 14:55:04 -0300

    Primeiro commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Felipe";
+        int idade = 13;
+
+        System.out.println("O dev "+ name +" de "+ idade +" anos, esta programando em java");
+    }
+}
