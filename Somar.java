52f986c6dc7fd51f8149e1c58e39cb8e1691e974
lipez <felipeirasoares@gmail.com>
2025-06-20 17:13:56 -0300

    Delete Somar.java

--- Somar.java
+++
@@ -1,9 +0,0 @@
-public class Somar {
-    public static void main(String[] args) {
-        int a = 20;
-        int b = 10;
-        int soma = a + b;
-
-        System.out.println("a soma entre "+ a +" mais "+ b +" e igual a "+ soma); // 30
-    }
-}
