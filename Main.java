047452acc0b1334ad362383f9774539727e3626b
lipez <felipeirasoares@gmail.com>
2025-06-20 17:11:03 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String name = "Felipe";
-        int idade = 13;
-
-        System.out.println("O dev "+ name +" de "+ idade +" anos, esta programando em java");
-    }
-}
